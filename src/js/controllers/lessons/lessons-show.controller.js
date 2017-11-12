74b90a1b76af21f38588c2f54f7e2cae3bb8c160
gjhughes <ghughes@me.com>
2017-11-12 13:45:02 +0000

    lesson show page added

---
+++ src/js/controllers/lessons/lessons-show.controller.js
@@ -0,0 +1,10 @@
+angular
+  .module('gaFeedback')
+  .controller('LessonsShowCtrl', LessonsShowCtrl);
+
+LessonsShowCtrl.$inject = ['Lesson', '$stateParams'];
+
+function LessonsShowCtrl(Lesson, $stateParams ) {
+  const vm = this;
+  vm.lessons = Lesson.get($stateParams);
+}
