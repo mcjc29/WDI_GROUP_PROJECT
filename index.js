74deb4e160ea97c1c44e30af52e4b113f28e6585
GWilman <georgewilman1@gmail.com>
2017-11-12 12:48:22 +0000

    url hash removed - $locationProvider

---
+++ index.js
@@ -0,0 +1,33 @@
+const express         = require('express');
+const morgan          = require('morgan');
+const bodyParser      = require('body-parser');
+const router          = require('./config/routes');
+const { db, port }    = require('./config/environment');
+const customResponses = require('./lib/customResponses');
+const errorHandler    = require('./lib/errorHandler');
+const cors            = require('cors');
+
+const app             = express();
+const environment     = app.get('env');
+
+const mongoose        = require('mongoose');
+mongoose.Promise      = require('bluebird');
+mongoose.connect(db[environment], { useMongoClient: true });
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
+
+app.use(customResponses);
+app.use('/api', router);
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.use(errorHandler);
+
+if(environment !== 'test') {
+  app.listen(port, () => console.log(`Express is up and running on port: ${port}`));
+}
+
+module.exports = app;
