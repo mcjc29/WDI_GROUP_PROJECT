ee34482f6865665e83c86ff06969282f6287d152
GWilman <georgewilman1@gmail.com>
2017-11-15 10:27:21 +0000

    chat in progress

--- src/js/controllers/chat/chat.controller.js
+++ src/js/controllers/chat/chat.controller.js
@@ -4,10 +4,11 @@ angular
 
 ChatCtrl.$inject = ['Comment', '$moment', '$rootScope', '$state'];
 
-function ChatCtrl(Comment, $moment, $rootScope, $state) {
+function ChatCtrl(Comment, $moment, $rootScope) {
   const vm = this;
   vm.submit = addComment;
   vm.delete = deleteComment;
+  vm.submitReply = addReply;
 
   $rootScope.$on('user defined', (event, data) => {
     vm.user = data.user;
@@ -39,17 +40,61 @@ function ChatCtrl(Comment, $moment, $rootScope, $state) {
           .$promise
           .then(data => {
             vm.comments = data.reverse();
+            vm.comments.forEach(comment => {
+              comment.createdAt = $moment(comment.createdAt).fromNow();
+              comment.replies.forEach(reply => {
+                reply.createdAt = $moment(reply.createdAt).fromNow();
+              });
+            });
+            vm.newComment = '';
           });
-        vm.newComment = '';
       });
   }
 
   function deleteComment(comment) {
     Comment
-      .remove({ id: comment.id })
+      .remove({ id: comment._id })
       .$promise
       .then(() => {
-        $state.go('studentsIndex');
+        Comment
+          .query()
+          .$promise
+          .then(data => {
+            vm.comments = data.reverse();
+            vm.comments.forEach(comment => {
+              comment.createdAt = $moment(comment.createdAt).fromNow();
+              comment.replies.forEach(reply => {
+                reply.createdAt = $moment(reply.createdAt).fromNow();
+              });
+            });
+          });
+      });
+  }
+
+  function addReply(comment) {
+    const newReply = {
+      createdBy: vm.user._id,
+      replies: {
+        content: vm.newReply
+      }
+    };
+    Comment
+      .update({ id: comment._id }, newReply)
+      .$promise
+      .then(() => {
+        Comment
+          .query()
+          .$promise
+          .then(data => {
+            vm.comments = data.reverse();
+            vm.comments.forEach(comment => {
+              comment.createdAt = $moment(comment.createdAt).fromNow();
+              comment.replies.forEach(reply => {
+                reply.createdAt = $moment(reply.createdAt).fromNow();
+              });
+            });
+            vm.newComment = '';
+          });
       });
   }
 
