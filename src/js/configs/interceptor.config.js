a790234ecf6489ec36a62f5f07f1761f3ddb848a
GWilman <georgewilman1@gmail.com>
2017-11-16 14:57:20 +0000

    user edit

--- src/js/configs/interceptor.config.js
+++ src/js/configs/interceptor.config.js
@@ -7,7 +7,7 @@ function Interceptor($httpProvider) {
   $httpProvider.interceptors.push(function() {
     return {
       request: function (config) {
-        if (config.url.startsWith('https://gagroupproject3.s3.eu-west-2.amazonaws.com/')) {
+        if (config.url.startsWith('https://ga-feedback-george.s3.eu-west-2.amazonaws.com/')) {
           delete config.headers.Authorization;
         }
         return config;
--- src/js/controllers/users/users-edit.controller.js
+++ src/js/controllers/users/users-edit.controller.js
@@ -8,12 +8,12 @@ function UsersEditCtrl(User, $stateParams, $state) {
   const vm = this;
 
   vm.user = User.get($stateParams);
-  vm.submit = user => {
+  vm.submit = () => {
     User
-      .update({ id: user.id }, user)
+      .update({ id: $stateParams.id }, vm.user)
       .$promise
-      .then(user => {
-        $state.go('usersShow', { id: user.id });
+      .then(() => {
+        $state.go('usersShow', { id: $stateParams.id });
       });
   };
 }
